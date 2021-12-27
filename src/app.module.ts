ca47a63e821d953ae4b91725f40ac2500d8f56b3
VergilGX <fabiosotomejia@gmail.com>
2021-12-27 17:46:36 -0500

    fix: Dependencies and Migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    BooksModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
